e03c483ff416cecb2d8d01f02766c3f52d1e98ff
如果的如果 <admin@fosuss.com>
2020-12-08 14:21:01 +0800

    优化

--- src/Scanner/Inotify.php
+++ src/Scanner/Inotify.php
@@ -14,25 +14,38 @@ class Inotify implements ScannerInterface
     public function __construct(WatchRule $rule)
     {
         $this->rule = $rule;
-        $this->inotifyResource = inotify_init();
-        stream_set_blocking($this->inotifyResource, false);
+        $this->init();
     }
 
     public function getChangeFiles(): array
     {
-        $watchItems = $this->rule->scan2Items();
-        $fileList = array_merge($watchItems->getFiles(), $watchItems->getPaths());
-
-        foreach ($fileList as $item) {
-            inotify_add_watch($this->inotifyResource, $item, IN_CREATE | IN_MODIFY | IN_DELETE | IN_MOVE);
-        }
-
         $ret = inotify_read($this->inotifyResource);
-        return $ret ? array_column($ret, 'name') : [];
+        $ret = $ret ? array_column($ret, 'name') : [];
+        if(!empty($ret)){
+            $this->init();
+        }
+        return $ret;
     }
 
     public function __destruct()
     {
-        fclose($this->inotifyResource);
+        if($this->inotifyResource){
+            fclose($this->inotifyResource);
+            $this->inotifyResource = null;
+        }
+    }
+
+    private function init()
+    {
+        if($this->inotifyResource){
+            fclose($this->inotifyResource);
+        }
+        $this->inotifyResource = inotify_init();
+        stream_set_blocking($this->inotifyResource, false);
+        $watchItems = $this->rule->scan2Items();
+        $fileList = array_merge($watchItems->getFiles(), $watchItems->getPaths());
+        foreach ($fileList as $item) {
+            inotify_add_watch($this->inotifyResource, $item, IN_CREATE | IN_MODIFY | IN_DELETE | IN_MOVE);
+        }
     }
 }
